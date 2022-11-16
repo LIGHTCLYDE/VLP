361b7228673fdf9b1a9a64c595cb4ca215e71875
The_jãoooo <78222249+LIGHTCLYDE@users.noreply.github.com>
2022-11-16 02:17:35 +0000

    Add files via upload

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,4 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navigation =
-document.querySelector('.navigation');
-menuToggle.onclick = () => {
-    navigation.classList.toggle('open');
-}
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
 
-const listItems = document.querySelectorAll('.list-item');
-listItems.forEach(item => {
-    item.onclick = () => {
-        listItems.forEach(item =>
-        item.classList.remove('active'));
-            item.classList.add('active');
-    }
-})
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
