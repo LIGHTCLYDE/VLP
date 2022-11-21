3373abce47b334939a54b268c87becd5de6c4972
The_jãoooo <78222249+LIGHTCLYDE@users.noreply.github.com>
2022-11-21 02:18:03 +0000

    Add files via upload

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,3 +6,14 @@ toggle.addEventListener('click', () => nav.classList.toggle('active'))
 
 //TRABALHOS//
 
+function rotate() {
+    var lastChild = $('.slider div:last-child').clone();
+    $('.slider div').removeClass('firstSlide')
+    $('.slider div:last-child').remove();
+    $('.slider').prepend(lastChild)
+    $(lastChild).addClass('firstSlide')
+}
+
+window.setInterval(function() {
+    rotate()
+}, 4000);
