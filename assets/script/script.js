eebed7a6fc7e5caa482bcdd15429b6d1341d7fa9
The_jãoooo <78222249+LIGHTCLYDE@users.noreply.github.com>
2022-12-12 22:35:43 -0300

    Att
    Att

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,17 +4,3 @@ $('#toggle').click(function() {
    });
 
 
-
-   //TRABALHOS//
-
-function rotate() {
-  var lastChild = $('.slider div:last-child').clone();
-  $('.slider div').removeClass('firstSlide')
-  $('.slider div:last-child').remove();
-  $('.slider').prepend(lastChild)
-  $(lastChild).addClass('firstSlide')
-}
-
-window.setInterval(function() {
-  rotate()
-}, 4000);
