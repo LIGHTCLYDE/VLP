b215d2e567f033079205ff22151c8f599a4fd9d1
The_jãoooo <78222249+LIGHTCLYDE@users.noreply.github.com>
2022-12-20 17:55:34 -0300

    Add files via upload

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -1,9 +1,12 @@
 function sendEmail() {
     Email.send({
-        SecureToken : "716b0632-bae7-49e5-97bd-d2cdc5d4e862",
+        
+        Host : "smtp.elasticemail.com",
+        Username : "volpitattoo@gmail.com",
+        Password : "vbhzduqegsrhutpz",
         To : 'volpitattoo@gmail.com',
         From : document.getElementById("email").value,
-        Subject : "Novo E-mail de contato recebido ",
+        Subject : "Novo E-mail de contato recebido",
         Body : "Name: " + document.getElementById("name").value
             + "<br> Email: " + document.getElementById("email").value
             + "<br> Telefone: " + document.getElementById("phone").value
